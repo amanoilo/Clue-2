bbf0e999bcf956ce0179ab76ea27b24c17088b45
contaylo <Contaylo@mymail.mines.edu>
2015-02-16 20:07:38 -0700

    Fixed calcTargets(), getTargets(), and getAdjList()

--- src/experiment/IntBoard.java
+++ src/experiment/IntBoard.java
@@ -5,12 +5,16 @@ import java.util.*;
 public class IntBoard {
 	private BoardCell[][] board;
 	private Map<BoardCell, LinkedList<BoardCell>> adj;
+	private Set<BoardCell> visited;
+	private Set<BoardCell> targets;
 	
 	public IntBoard(){
 		board = new BoardCell[4][4];
 		adj = new HashMap<BoardCell, LinkedList<BoardCell>>();
-		for (int i = 0; i < 4; i++){
-			for (int j = 0; j < 4; j++){
+		visited = new HashSet<BoardCell>();
+		targets = new HashSet<BoardCell>();
+		for (int i = 0; i < board.length; i++){
+			for (int j = 0; j < board[i].length; j++){
 				BoardCell cell = new BoardCell(i,j);
 				board[i][j] = cell;
 			}
@@ -42,15 +46,28 @@ public class IntBoard {
 	}
 	
 	public void calcTargets(BoardCell start, int moves){
-		Set<BoardCell> visited = new HashSet<BoardCell>(); //this probably ends up being somthing that goes at the top
+		if(moves == 0){
+			targets.add(start);
+		}
+		else{
+			if(!visited.contains(start)){
+				visited.add(start);
+				for(BoardCell bc : getAdjList(start)){
+					calcTargets(bc, moves-1);
+				}
+			}
+		}
 	}
 	
-	public Set<BoardCell> getTargets(){
-		return new HashSet<BoardCell>();
+	public Set<BoardCell> getTargets(BoardCell start, int moves){
+		targets.clear();
+		visited.clear();
+		calcTargets(start, moves);
+		return targets;
 	}
 	
 	public LinkedList<BoardCell> getAdjList(BoardCell cell){
-		return new LinkedList<BoardCell>();
+		return adj.get(cell);
 	}
 	
 	
